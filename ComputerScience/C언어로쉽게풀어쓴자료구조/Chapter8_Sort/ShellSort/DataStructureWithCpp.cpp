72f74258bbad044c4e057350ec8057377b5a5d83
yjchoi7166 <yjchoi7166@urbanbase.com>
2021-02-07 00:28:33 +0900

    [feat] Added Shell Sort

---
+++ ComputerScience/C언어로쉽게풀어쓴자료구조/Chapter8_Sort/ShellSort/DataStructureWithCpp.cpp
@@ -0,0 +1,44 @@
+﻿#include "ShellSort.h"
+#include <iostream>
+using namespace std;
+
+
+template<typename T>
+void PrintVector(const vector<T>& arr) {
+	for (const T& a : arr) {
+		cout << a << " ";
+	}
+	cout << endl;
+}
+
+
+
+int lessComp(const int& a, const int& b) {
+	return a < b;
+}
+
+int greaterComp(const int& a, const int& b) {
+	return a > b;
+}
+
+
+void ShellSortExample() {
+	vector<int> data{ 13,21,9,100,15,88,56,43,77,101 };
+
+	ShellSort::Sort(data, lessComp);
+	PrintVector(data);
+
+	ShellSort::Sort(data, greaterComp);
+	PrintVector(data);
+
+}
+
+
+
+int main()
+{
+
+	ShellSortExample();
+
+	return 0;
+}
