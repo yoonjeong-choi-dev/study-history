97b86fddcd0383de651a6f9e9defbde789685162
yjchoi7166 <yjchoi7166@urbanbase.com>
2021-02-06 23:59:42 +0900

    [feat] Added Insertion Sort

---
+++ ComputerScience/C언어로쉽게풀어쓴자료구조/Chapter8_Sort/InsertionSort/InsertionSort.h
@@ -0,0 +1,45 @@
+#ifndef __INSERTION_SORT_H__
+#define __INSERTION_SORT_H__
+
+
+#include <vector>
+
+class InsertionSort {
+	template<typename T>
+	static void swap(T& a, T& b) {
+		T temp = a;
+		a = b;
+		b = temp;
+	}
+
+public:
+	template <typename T>
+	static void Sort(std::vector<T>& arr, int (*comp)(const T& a, const T& b)) {
+		int size = arr.size();
+
+		T insertData;
+		int insertIdx;
+		// arr[i]가 삽입되어야 하는 위치를 arr[0]~arr[i-1]에서 찾는다
+		for (int i = 1; i < size; i++) {
+			insertData = arr[i];
+			for (insertIdx = i-1; insertIdx >= 0; insertIdx--) {
+				// 삽입되어야 할 데이터가 arr[insertIdx] 보다 먼저 와야 하는 경우
+				// => arr[insertIdx] 를 뒤로 보내준다
+				if (comp(insertData, arr[insertIdx]) == 1) {
+					swap(arr[insertIdx], arr[insertIdx + 1]);
+				}
+				// 삽입 위치(insertIdx+1)를 찾은 경우 끝
+				else {
+					break;
+				}
+			}
+
+			// 삽입 위치에 현재 데이터 넣는다
+			arr[insertIdx+1] = insertData;
+		}
+	}
+};
+
+
+#endif // !__INSERTION_SORT_H__
+
