8cf607c43804ea88f9d38b2bbb6fabf2d43167ef
yjchoi7166 <yjchoi7166@urbanbase.com>
2021-02-07 23:13:30 +0900

    [feat] Added Merge Sort

---
+++ ComputerScience/C언어로쉽게풀어쓴자료구조/Chapter8_Sort/MergeSort/DataStructureWithCpp.cpp
@@ -0,0 +1,44 @@
+﻿#include "MergeSort.h"
+#include <iostream>
+using namespace std;
+
+
+template<typename T>
+void PrintVector(const vector<T>& arr) {
+	for (const T& a : arr) {
+		cout << a << " ";
+	}
+	cout << endl;
+}
+
+
+
+int lessComp(const int& a, const int& b) {
+	return a < b;
+}
+
+int greaterComp(const int& a, const int& b) {
+	return a > b;
+}
+
+
+void MergeSortExample() {
+	vector<int> data{ 13,21,9,100,15,88,56,43,77,101,4,121,500,34 };
+
+	MergeSort::Sort(data, lessComp);
+	PrintVector(data);
+
+	MergeSort::Sort(data, greaterComp);
+	PrintVector(data);
+
+}
+
+
+
+int main()
+{
+
+	MergeSortExample();
+
+	return 0;
+}
