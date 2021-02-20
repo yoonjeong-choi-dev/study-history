80c7be938467ac90e3e4f48ce8e4dbb414a71c0d
yjchoi7166 <yjchoi7166@urbanbase.com>
2021-02-20 17:51:39 +0900

    [feat] Added Sequential Search

---
+++ ComputerScience/C언어로쉽게풀어쓴자료구조/Chapter12_Search/SequentialSearch/DataStructureWithCpp.cpp
@@ -0,0 +1,51 @@
+﻿#include "SequentialSearch.h"
+
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+
+int main()
+{
+
+	vector<int> test = { 1,5,6,2,123,534,23,42,100,94,23 };
+
+	int ret, target;
+
+
+	cout << "Sequential Search : " << endl;
+
+	target = 123;
+	ret = SequentialSearch<int>::Search(test, target);
+	cout << target<< " is in " << ret << endl;
+
+	target = 1000;
+	ret = SequentialSearch<int>::Search(test, target);
+	cout << target << " is in " << ret << endl;
+
+	target = 42;
+	ret = SequentialSearch<int>::Search(test, target,0,3);
+	cout << target << " is in " << ret << endl;
+
+
+	cout << "Improved Search : " << endl;
+
+	target = 123;
+	ret = SequentialSearch<int>::ImprovedSearch(test, target);
+	cout << target << " is in " << ret << endl;
+
+	target = 1000;
+	ret = SequentialSearch<int>::ImprovedSearch(test, target);
+	cout << target << " is in " << ret << endl;
+
+	target = 42;
+	ret = SequentialSearch<int>::ImprovedSearch(test, target, 0, 3);
+	cout << target << " is in " << ret << endl;
+
+
+
+	return 0;
+}
