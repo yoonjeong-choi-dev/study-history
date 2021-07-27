3bbb5215a74cae7bfc122246134ceee929a1c4ee
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2021-07-27 16:10:32 +0900

    [feat] ch1 simple-server 추가

---
+++ ComputerScience/InfraStructure/BuildingMicroServiceWithGo/ch1-intro-msa/simple-hello-server/simple-hello-server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World\n")
+}
+
+func main() {
+	port := 8080
+
+	// 핸들러 등록 : 라우팅
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	// 서버 오픈
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
