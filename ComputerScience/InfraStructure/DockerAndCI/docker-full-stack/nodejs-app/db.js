e06534916e89c8a321dbbb185195fcbcc05d02c8
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2021-12-25 03:45:36 +0900

    [feat] Added Chapter 9. Travis CI with AWS
    - 운영 환경에 배포하기 위해 애플리케이션 소스 변경
      - mysql 관련 파일 및 컨테이너 불필요
      - db 설정 변경
        - nodejs에서 db 연결 정보를 환경 변수를 통해 설정
        - 서버 실행 시, db에 테이블 생성
    - Travis-CI 설정을 위한 .travis.yml 작성
      - 테스트를 위한 임시 깃 레파지토리 생성
      - https://github.com/yoonjeong-choi-dev/fullstack-container-ci-test
      - 도커 계정 및 AWS 설정 관련 변수들은 travis 환경 변수 사용
    - 운영 용 도커 컴포즈 작성
      - 이미지를 빌드하는 대신, travis-ci를 통해 빌드하고 푸시한 도커 허브 이미지 사용
    - AWS 세팅
      - vpc는 디폴트 사용
        - eb의 경우 ec2 인스턴스를 통해 확인 가능
        - RDS와 EB와 동일 vpc로 설정
      - 보안 그룹
          - 동일 VPC 내 서비스 사이의 트래픽 정의
          - db 접속 관련 트래픽 제어
      - RDS는 nodejs 앱과 연결
        - RDS 접속 관련 변수들은 일라스틱 빈스톡의 환경 속성을 통해 설정

--- ComputerScience/InfraStructure/DockerAndCI/docker-full-stack/nodejs-app/db.js
+++ ComputerScience/InfraStructure/DockerAndCI/docker-full-stack/nodejs-app/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql');
 
 // TODO : 데이터베이스 연결 관련 정보들은 환경 변수로 대체 필요
-const pool = mysql.createPool({
+// 개발용과 같이 쓰기 위해 운영용 환경변수 가없는 경우 개발 용 변수 할당
+exports.pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',  // 도커 컴포즈에서 정의한 db 서비스 이름
-    user: 'root',
-    password: 'yjdbpassword',
-    database: 'yjApp',
-    port: 3306,
+    host: process.env.MYSQL_HOST || 'mysql',  // 도커 컴포즈에서 정의한 db 서비스 이름
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_ROOT_PASSWORD || 'yjdbpassword',
+    database: process.env.MYSQL_DATABASE || 'yjApp',
+    port: process.env.MYSQL_PORT || 3306,
 });
-
-exports.pool = pool;
--- ComputerScience/InfraStructure/DockerAndCI/docker-full-stack/nodejs-app/index.js
+++ ComputerScience/InfraStructure/DockerAndCI/docker-full-stack/nodejs-app/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const db = require('./db');
 
-const port = 5000;
+const port = 5123;
 
 const app = express();
 
 // 미들웨어 설정
 app.use(express.json());
+
+// 테이블 생성
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT, 
+    PRIMARY KEY (id)
+)`, (err, results, fileds) => {
+    console.log('results', results)
+});
 
 // API 구현
 app.get('/api/values', (req, res) => {
